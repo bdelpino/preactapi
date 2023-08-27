415c62207043c23979169586a0c6d7f0c6e36e1e
bdelpino <bastian.delpinoa@gmail.com>
2023-08-27 00:47:37 -0400

    MiApi funcionality achieved

--- p-react-api-l/src/App.jsx
+++ p-react-api-l/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import MiApi from './components/MiApi.jsx';
 
 function App() {
   
   return (
     <>
+              <div>
+                      <div>
+                            <h1>The Naruto Database</h1>
+                      </div>
 
+                      <div>
+                            <MiApi/>
+                      </div>
+
+              </div>
     </>
   )
 }
--- p-react-api-l/src/components/MiApi.jsx
+++ p-react-api-l/src/components/MiApi.jsx
@@ -1,14 +1,39 @@
 // /src/components/MiApi.jsx
 
-const MiApi     =   ()  =>  {
+import {useState,useEffect} from 'react'
+
+const MiApi = ()  =>  {
+
+                                const [infor,setInfor] = useState("character")
+                                const [informations,setInformations] = useState([])
+                                const consultaApi = async () => {
+                                                                    
+                                                                    const url = `https://www.narutodb.xyz/api/${infor}?limit=1500`;
+                                                                    const response = await fetch (url);
+                                                                    const data = await response.json();
+                                                                    
+                                                                    const test = infor + "s";
+                                                                    setInformations(data[test]);
+                                                                }
+
+                                useEffect   (   ()  =>  {
+                                                            consultaApi()
+                                                            
+                                                        }, []
+
+
+                                            )
 
                                 return  (
-                                            console.log('hola')
+                                            
 
+                                            <ul>
+                                                    {informations.map(information => <li key={information.id}>{information.id}</li>)}
+                                            </ul>
                                         )
 
 
 
-                            }
+                        }
 
 export default MiApi
